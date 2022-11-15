09c807fb9d41c6a8feb1280edc7e3471e547c23c
Cosmin Negrut <cosminnegrut776@gmail.com>
2022-11-15 21:36:04 +0200

    prevent app to refresh on form submit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,19 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  state = { term: "dvd" };
+  state = { term: "" };
   onInputChange = (event) => {
     this.setState({ term: event.target.value });
+  };
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    //TODO:Make sure we call callback from parent component
   };
 
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label type="text">Video Search</label>
             <input
