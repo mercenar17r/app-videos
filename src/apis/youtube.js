1652c15f97f20d3a596d3fd2df8bfef916491b37
Cosmin Negrut <cosminnegrut776@gmail.com>
2022-11-15 22:05:36 +0200

    configure axios

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
 const KEY = "AIzaSyBmnQ8BCNyNfcz63ydVCF_nDQtwQ6bv3Rk";
+
+export default axios.create({
+  baseURL: " https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY,
+  },
+});
